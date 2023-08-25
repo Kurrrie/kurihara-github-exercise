73db6b2eee2db4d8e87bdfb4491d07d19fc359bd
Kurrrie <143159335+Kurrrie@users.noreply.github.com>
2023-08-25 14:55:39 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    a += 1
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    if (b_b == 0) {
+        b += 1
+        b_b = 1
+    } else if (false) {
+    	
+    }
+})
+let b_b = 0
+let a = 0
+let b = 0
+basic.forever(function () {
+	
+})
